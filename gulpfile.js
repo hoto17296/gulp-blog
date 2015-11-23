6a7e9b67672240510ac2fed09f0817ded53e4e0b
ほと <mail@hoto.me>
2015-11-23 18:11:24 +0900

    install gulp plugins

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,6 @@
-var gulp = require('gulp');
+var gulp        = require('gulp');
+var markdown    = require('gulp-markdown');
+var rename      = require('gulp-rename');
+var layout      = require('gulp-layout');
+var frontmatter = require('gulp-frontmatter');
+var webserver   = require('gulp-webserver');
