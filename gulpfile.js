7939146c8b0b31cc6b7c9250bdc569c47ce3196d
ほと <mail@hoto.me>
2015-11-24 08:09:02 +0900

    ビルドタスクを一通り実装する TODO clean を同期的に実行するようにしたい

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,47 @@
 var gulp        = require('gulp');
-var markdown    = require('gulp-markdown');
-var rename      = require('gulp-rename');
-var layout      = require('gulp-layout');
-var frontmatter = require('gulp-frontmatter');
 var webserver   = require('gulp-webserver');
+var frontMatter = require('gulp-front-matter');
+var markdown    = require('gulp-markdown');
+var layout      = require('gulp-layout');
+var rename      = require('gulp-rename');
+var rimraf      = require('rimraf');
+
+gulp.task('default', ['build', 'watch', 'server']);
+
+gulp.task('build', ['clean', 'images', 'posts']);
+
+gulp.task('watch', () => {
+  gulp.watch('src/_posts/*.md', ['posts']);
+  gulp.watch('src/images/**/*.{jpg,jpeg,gif,png,svg}', ['images'])
+});
+
+gulp.task('server', () => {
+  gulp.src('public')
+    .pipe(webserver({
+    }));
+});
+
+gulp.task('clean', (cb) => rimraf('public', cb));
+
+gulp.task('posts', () => {
+  return gulp.src('src/_posts/*.md')
+    .pipe(frontMatter())
+    .pipe(markdown())
+    .pipe(layout((file) => {
+      opts = file.frontMatter;
+      opts.layout = `src/_layouts/${opts.layout||'post'}.jade`;
+      return opts;
+    }))
+    .pipe(rename((path) => {
+      path.dirname += '/' + path.basename;
+      path.basename = 'index';
+      path.extname = '.html';
+      return path;
+    }))
+    .pipe(gulp.dest('public'));
+});
+
+gulp.task('images', () => {
+  return gulp.src('src/images/**/*.{jpg,jpeg,gif,png,svg}')
+    .pipe(gulp.dest('public/images'));
+});
